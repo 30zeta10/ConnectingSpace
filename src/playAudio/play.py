3b0cfc87b0fcffc7599209cc8307b5adf0d067c1
Toni Wirth <toni.wirth@fu-berlin.de>
2018-01-09 19:36:42 +0100

    Python Audio Play Skript

---
+++ src/playAudio/play.py
@@ -0,0 +1,25 @@
+import pyaudio
+import wave
+import sys
+
+if (len(sys.argv) < 2):
+    print("Agument failure")
+    sys.exit(-1)
+
+wa = wave.open(sys.argv[1], 'rb')
+play = pyaudio.PyAudio()
+
+stream = play.open(format = play.get_format_from_width(wa.getsampwidth()),
+                    channels = wa.getnchannels(),
+                    rate = wa.getframerate(),
+                    output = True)
+
+daten = wa.readframes(1024)
+
+while daten != '':
+    stream.write(daten)
+    daten = wa.readframes(1024)
+
+stream.stop_stream()
+stream.close()
+play.terminate()
