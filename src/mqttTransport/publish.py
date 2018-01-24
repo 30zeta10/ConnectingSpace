537a6e181b7a3cb9c8589ba2124e477a11f6cb3b
Toni Wirth <toni.wirth@fu-berlin.de>
2018-01-24 10:34:43 +0100

    publisher und subscriber skript getrennt

---
+++ src/mqttTransport/publish.py
@@ -0,0 +1,28 @@
+import paho.mqtt.client as mqtt
+
+topic = "/voice/"
+user = "pdqmkvuw"
+pw = "G4CeJE2Qs-rh"
+host = "m23.cloudmqtt.com"
+port = 16533
+
+
+def on_message(client, obj, msg):
+    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+
+mqttc = mqtt.Client()
+
+mqttc.on_message = on_message
+
+mqttc.username_pw_set(user, pw)
+mqttc.connect(host, port)
+
+#mqttc.subscribe(topic, 0)
+
+mqttc.publish(topic, "YEEEEEEES")
+
+mqttc.loop_stop()
+#rc = 0
+#while rc == 0:
+#    rc = mqttc.loop()
+#print("rc: " + str(rc))
--- src/mqttTransport/subscripe.py
+++ src/mqttTransport/subscripe.py
@@ -6,7 +6,6 @@ pw = "G4CeJE2Qs-rh"
 host = "m23.cloudmqtt.com"
 port = 16533
 
-
 def on_message(client, obj, msg):
     print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
 
@@ -14,18 +13,14 @@ mqttc = mqtt.Client()
 
 mqttc.on_message = on_message
 
-# Connect
 mqttc.username_pw_set(user, pw)
 mqttc.connect(host, port)
 
-# Start subscribe, with QoS level 0
 mqttc.subscribe(topic, 0)
 
-# Publish a message
-mqttc.publish(topic, "YEEEEEEES")
-
-# Continue the network loop, exit when an error occurs
+mqttc.loop_stop()
 rc = 0
+
 while rc == 0:
     rc = mqttc.loop()
 print("rc: " + str(rc))
